8015a1d4b4d73df0332916eae371e15477fa9767
Juan Pablo <67966567+juannajup@users.noreply.github.com>
2022-07-15 09:48:52 -0300

    ccc

--- src/main/java/servlets/MecanicaServlet.java
+++ src/main/java/servlets/MecanicaServlet.java
@@ -1,9 +1,6 @@
 package servlets;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
